0fdb389f8c9a85511ca8ead87bfa7f1a79e5a3d7
Phuoc Pham <phuocphn@gmail.com>
2025-04-11 12:29:33 +0200

    feat: add Hydra config and split pair structure identification prompts into multiple parts
    - Integrated Hydra for flexible and modular configuration management.
    - Refactored prompt logic to break down pair structure identification into smaller, more manageable sub-prompts.
    - Improved readability, maintainability, and control over model interactions.
    - Enabled easier tuning and experimentation with config-driven workflows.

--- main.py
+++ main.py
@@ -10,7 +10,11 @@ from langchain_ollama import  ChatOllama
 from langchain_openai import ChatOpenAI
 
 from calc1 import compute_cluster_metrics, compute_cluster_metrics_hl1, average_metrics
-from netlist import SPICENetlist
+from src.netlist import SPICENetlist
+from src.kb import get_knowledge_base
+import hydra
+from omegaconf import DictConfig, OmegaConf
+
 
 #-----
 from loguru import logger
@@ -93,16 +97,16 @@ def create_prompt_hl2():
     return prompt
 
 
-def create_prompt_hl2_current_mirrors_only():
+def create_prompt_hl2_with_target_single_subcircuit_only(subcircuit_name = "Current Mirror", abbreviation = "CM"):
     prompt = ChatPromptTemplate.from_messages(
         [
             (
                 "system",
-                "You are an experienced analog circuit designer. Given a SPICE netlist, your task is to identify and extract available current mirrors (CM)." +  
+                f"You are an experienced analog circuit designer. Given a SPICE netlist, your task is to identify and extract available {subcircuit_name} ({abbreviation})." +  
                 "Provide your output in JSON format, " + 
                 "as a list of dictionaries. Each dictionary must contain two keys: " + "\n" +
-                "- 'sub_circuit_name': 'CM' " + "\n" +
-                "- 'transistor_names': a list of transistor names that belong to this current mirror" + "\n" +
+                f"- 'sub_circuit_name': '{abbreviation}' " + "\n" +
+                f"- 'transistor_names': a list of transistor names that belong to this {subcircuit_name}" + "\n" +
                 "Wrap your response between <json> and </json> tags. Do not output any further explanation, description, or comments." + "\n" 
             ),
             ("human", "Input SPICE netlist\n{netlist} \nLet's think step-by-step."),
@@ -110,86 +114,148 @@ def create_prompt_hl2_current_mirrors_only():
     )
     return prompt
 
+def create_prompt_hl2_with_target_single_subcircuit_only_and_fixed_rule_provided(subcircuit_name = "Current Mirror", abbreviation = "CM"):
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            (
+                "system",
+                f"You are an experienced analog circuit designer. Given a SPICE netlist, your task is to identify and extract available {subcircuit_name} ({abbreviation})." +  
+                f"When you answer the questions, try to use the provided definition, connection rules, identification procedure to identify {subcircuit_name}." + 
+                "Provide your output in JSON format, " + 
+                "as a list of dictionaries. Each dictionary must contain two keys: " + "\n" +
+                f"- 'sub_circuit_name': '{abbreviation}' " + "\n" +
+                f"- 'transistor_names': a list of transistor names that belong to this {subcircuit_name}" + "\n" +
+                "Wrap your response between <json> and </json> tags. Do not output any further explanation, description, or comments." + "\n" 
+
+                f"Knowledge Base: \n {get_knowledge_base()[abbreviation]}" +  "\n" 
+            ),
+            ("human", "Input SPICE netlist\n{netlist} \nLet's think step-by-step."),
+        ]
+    )
+    return prompt
+
+def llm_invoke(model, prompt, data):
+    try:
+        # prompt = create_prompt_hl2()
+        logger.info(prompt.invoke(data.netlist).to_string())
+        chain = prompt | model #| parser
+        output = chain.invoke({"netlist": data.netlist})
+        parsed_data = json.loads(output.content[output.content.find("<json>") + len("<json>"):output.content.find("</json>")])
+        assert isinstance(parsed_data, list), "parsed data is not a list"
+        return output, parsed_data
+    except json.decoder.JSONDecodeError as e:
+        logger.error (f"parsing LLM output failed")
+        return None, None
+
+    except Exception as e:
+        logger.error (f"exception: {e}")
+        return None, None
+
 def identify_devices(subset="medium", model=None, prompt=None, category="single"):
     results = []
     i = 1
     max_i  = 101
 
     num_attempts = 0
-    max_attempts = 30
+    max_attempts = 5
 
     while i <  max_i and num_attempts < max_attempts:
         data = SPICENetlist(f"data/benchmark-asi-100/{subset}/{i}/")
         logger.info ("netlist #" + str(i))
-        try:
-            # prompt = create_prompt_hl2()
-            logger.info(prompt.invoke(data.netlist).to_string())
-            chain = prompt | model #| parser
-            output = chain.invoke({"netlist": data.netlist})
 
+        try:
+            if len(prompt) == 1:
+                output, parsed_data = llm_invoke(model, prompt[0], data)
+                parsed_data = parsed_data
+            else:
+                parsed_data = []
+                output = []
+                for p in prompt:
+                    partial_output, partial_parsed_data = llm_invoke(model, p, data)
+                    if output is not None and partial_parsed_data is not None:
+                        parsed_data += partial_parsed_data
+                        output.append(partial_output.content)
+                output = "\n".join(output)
         except Exception as e:
             logger.error (f"exception: {e}")
-            return
-        
+            num_attempts += 1
+            continue
+                
+
         logger.info (f"# output={output}")
 
-        try:
-            predicted_output = json.loads(output.content[output.content.find("<json>") + len("<json>"):output.content.find("</json>")])
     
-            logger.info ("------------------------------------")
-            logger.info (f"predicted_output: {json.dumps(predicted_output, indent=2)}")
+        logger.info ("------------------------------------")
+        logger.info (f"predicted_output: {json.dumps(parsed_data, indent=2)}")
 
-            if category == "single":
-                logger.info (f"ground truth: {json.dumps(data.hl1_gt, indent=2)}")
-                eval_results = compute_cluster_metrics_hl1(predicted=predicted_output, ground_truth=data.hl1_gt)
-            elif category == "pair":
-                logger.info (f"ground truth: {json.dumps(data.hl2_gt, indent=2)}")
-                eval_results = compute_cluster_metrics(predicted=predicted_output, ground_truth=data.hl2_gt)
-            else:
-                logger.error (f"unknown category: {category}")
-                return
+        if category == "single":
+            logger.info (f"ground truth: {json.dumps(data.hl1_gt, indent=2)}")
+            eval_results = compute_cluster_metrics_hl1(predicted=parsed_data, ground_truth=data.hl1_gt)
+        elif category == "pair":
+            logger.info (f"ground truth: {json.dumps(data.hl2_gt, indent=2)}")
+            eval_results = compute_cluster_metrics(predicted=parsed_data, ground_truth=data.hl2_gt)
+        else:
+            logger.error (f"unknown category: {category}")
+            return
 
-            logger.info (f"{eval_results=}")
-            logger.info ("------------------------------------")
-            results.append(eval_results)
+        logger.info (f"{eval_results=}")
+        logger.info ("------------------------------------")
+        results.append(eval_results)
 
-            num_attempts = 0
-        except json.decoder.JSONDecodeError as e:
-            logger.error (f"parsing LLM output failed, retry {num_attempts}...")
-            num_attempts += 1
-            continue
-        except Exception as e:
-            logger.exception (f"exception: {e}")
-            logger.error (f"could not compute comparison metrics, retry {num_attempts}...")
-            num_attempts += 1
-            continue
+        num_attempts = 0
 
         i += 1
-    return average_metrics(results)
+    #return average_metrics(results)
+    return results
 
-logger.info ("\n\n\n")
 
-llm_models = [
-    "deepseek-r1:70b",
-    "llama3.3:70b",
-    "llama3:70b",
-    "llama3:70b-instruct",
-    "mistral:7b-instruct",
-    "mixtral:8x22b"
-]
+@hydra.main(version_base=None, config_path="conf", config_name="config")
+def main(config : DictConfig) -> None:
 
-llm_models = [
-    "llama3:70b",
-]
+    logger.info (OmegaConf.to_yaml(config) + "\n\n\n")
 
-subset = "small" # "small", "medium", "large"
-for model_id in llm_models:
-    llm =load_ollama(model_id)
-    for category in ["single", "pair"]:
-        if category == "single":
-            prompt = create_prompt_hl1()
-        elif category == "pair":
-            prompt = create_prompt_hl2()
+    if config.eval_llms == "small":
+        llm_models = config.small_llms
+    elif config.eval_llms == "medium":
+        llm_models = config.medium_llms
+    elif config.eval_llms == "all":
+        llm_models = config.all_llms
 
-        result = identify_devices(subset, llm, prompt=prompt, category=category)
-        logger.info (f"**result**: model={model_id}, category={category}:   {result}")
+
+    input("Press Enter to continue...")
+
+    # get current time as a string for file name
+    now = datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
+
+    for subset in config.benchmark_subsets:
+        for model_id in llm_models:
+            llm =load_ollama(model_id)
+            for category in config.categories: #["single", "pair"]:
+                if category == "single":
+                    prompt = create_prompt_hl1()
+                    result = average_metrics(identify_devices(subset, llm, prompt=[prompt], category=category))
+
+                elif category == "pair":
+                    if config.break_hl2_prompt:
+                        prompts = []
+                        for subcircuit_name, abbreviation in config.subcircuits.items():
+                            if config.rule_provided:
+                                prompt = create_prompt_hl2_with_target_single_subcircuit_only_and_fixed_rule_provided(subcircuit_name=subcircuit_name, abbreviation=abbreviation)
+                                prompts.append(prompt)
+                            else:
+                                prompt = create_prompt_hl2_with_target_single_subcircuit_only(subcircuit_name=subcircuit_name, abbreviation=abbreviation)
+                                prompts.append(prompt)
+
+                        result = average_metrics(identify_devices(subset, llm, prompt=prompts, category=category))
+                    else:
+                        prompt = create_prompt_hl2()
+                        result = average_metrics(identify_devices(subset, llm, prompt=[prompt], category=category))
+
+                logger.info (f"**result**: model={model_id},category={category}:   {result}")
+
+                # write result to file
+                with open(f"results/{now}.txt", "a") as fw:
+                    fw.write(f"model={model_id},subset={subset},category={category}:   {result}\n")
+
+if __name__ == "__main__":
+    main()
