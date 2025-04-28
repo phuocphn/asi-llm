0aead699a3aaacf1e109dd59c02f09b789090e7d
Phuoc Pham <phuocphn@gmail.com>
2025-04-28 23:34:02 +0200

    :sparkles: add hl3 ground truth labels

--- src/extract_circuit_info.py
+++ src/extract_circuit_info.py
@@ -4,99 +4,167 @@ import os
 from collections import defaultdict
 
 rename_map = {
-	'MosfetCascodedPMOSAnalogInverter': 'Inverter',
-	'MosfetCascodedNMOSAnalogInverter': 'Inverter',
-	'MosfetCascodedAnalogInverter': 'Inverter',
-	'MosfetCascodeAnalogInverterPmosDiodeTransistor': 'Inverter',
-	'MosfetCascodeAnalogInverterNmosDiodeTransistor': 'Inverter',
-	'MosfetCascodeNMOSAnalogInverterOneDiodeTransistor': 'Inverter',
-	"MosfetCascodePMOSAnalogInverterOneDiodeTransistor": "Inverter",
-	"MosfetCascodePMOSAnalogInverterCurrentMirrorLoad": "Inverter",
-	"MosfetCascodeNMOSAnalogInverterCurrentMirrorLoad": "Inverter",
-	"MosfetCascodeAnalogInverterTwoCurrentMirrorLoads": "Inverter",
-	'MosfetAnalogInverter': 'Inverter',
-	'MosfetDifferentialPair': 'DiffPair',
-	'MosfetFoldedCascodeDifferentialPair': 'DiffPair',
-	"MosfetCascodedDifferentialPair": "DiffPair",
-	'MosfetSimpleCurrentMirror': 'CM',
-	'MosfetImprovedWilsonCurrentMirror': 'CM',
-	"MosfetCascodeAnalogInverterPmosCurrentMirrorLoad": "Inverter",
-	"MosfetCascodeAnalogInverterNmosCurrentMirrorLoad": "Inverter",
-	"MosfetFourTransistorCurrentMirror": "CM",
-	"MosfetCascodeCurrentMirror": "CM",
-	"MosfetWilsonCurrentMirror": "CM",
-	"MosfetWideSwingCascodeCurrentMirror": "CM",
-	"InverterPmosCurrentMirrorLoad": "CM",
-	'CapacitorArray': 'cap',
-	'MosfetDiodeArray': 'MosfetDiode',
-	'MosfetNormalArray': 'Mosfet'
+    "MosfetCascodedPMOSAnalogInverter": "Inverter",
+    "MosfetCascodedNMOSAnalogInverter": "Inverter",
+    "MosfetCascodedAnalogInverter": "Inverter",
+    "MosfetCascodeAnalogInverterPmosDiodeTransistor": "Inverter",
+    "MosfetCascodeAnalogInverterNmosDiodeTransistor": "Inverter",
+    "MosfetCascodeNMOSAnalogInverterOneDiodeTransistor": "Inverter",
+    "MosfetCascodePMOSAnalogInverterOneDiodeTransistor": "Inverter",
+    "MosfetCascodePMOSAnalogInverterCurrentMirrorLoad": "Inverter",
+    "MosfetCascodeNMOSAnalogInverterCurrentMirrorLoad": "Inverter",
+    "MosfetCascodeAnalogInverterTwoCurrentMirrorLoads": "Inverter",
+    "MosfetAnalogInverter": "Inverter",
+    "MosfetDifferentialPair": "DiffPair",
+    "MosfetFoldedCascodeDifferentialPair": "DiffPair",
+    "MosfetCascodedDifferentialPair": "DiffPair",
+    "MosfetSimpleCurrentMirror": "CM",
+    "MosfetImprovedWilsonCurrentMirror": "CM",
+    "MosfetCascodeAnalogInverterPmosCurrentMirrorLoad": "Inverter",
+    "MosfetCascodeAnalogInverterNmosCurrentMirrorLoad": "Inverter",
+    "MosfetFourTransistorCurrentMirror": "CM",
+    "MosfetCascodeCurrentMirror": "CM",
+    "MosfetWilsonCurrentMirror": "CM",
+    "MosfetWideSwingCascodeCurrentMirror": "CM",
+    "InverterPmosCurrentMirrorLoad": "CM",
+    "CapacitorArray": "cap",
+    "MosfetDiodeArray": "MosfetDiode",
+    "MosfetNormalArray": "Mosfet",
 }
 
 
 def rename(circuit_name):
-	# remove indexing-brackets: MosfetNormalArray[19] -> MosfetNormalArray
-	circuit_name = circuit_name[:circuit_name.find("[")]
+    # remove indexing-brackets: MosfetNormalArray[19] -> MosfetNormalArray
+    circuit_name = circuit_name[: circuit_name.find("[")]
 
-	for old_name, new_name in rename_map.items():
-		circuit_name = circuit_name.replace(old_name, new_name)
-	return circuit_name
-
-
-def extract_HL2_devices(subcircuits):
-	HL2_subcircuits = []
-	for sc in subcircuits:
-		name = rename(sc.attrib['name'])
-		if name == "DiffPair":
-			should_skip = False
-			for pin in  sc[0]:
-				#print (pin.attrib['name'], pin.attrib['net'])
-				if pin.attrib['net'] == "/vref":
-					should_skip = True
-					break
-			if should_skip:
-				continue
-
-
-		device_names = []
-		for device in sc.iter('device'):
-			device_names.append(device.attrib['name'].replace("/", ""))
-		
-		if name not in ["cap", "MosfetDiode", "Mosfet"]:
-			if len(device_names) == 1:
-				print ("Warning: Only one device in subcircuit:", name)
-				print (sc.attrib['name'])
-			HL2_subcircuits.append( {'sub_circuit_name': name, 'transistor_names': device_names})
-	return HL2_subcircuits
+    for old_name, new_name in rename_map.items():
+        circuit_name = circuit_name.replace(old_name, new_name)
+    return circuit_name
 
 
 def get_hl1_cluster_labels(netlist_dir="data/netlist1/"):
-	tree = ET.parse(glob.glob(os.path.join(netlist_dir, "structure_result.xml"))[0])
-	root = tree.getroot()
-	subcircuits = root[1]
+    tree = ET.parse(glob.glob(os.path.join(netlist_dir, "structure_result.xml"))[0])
+    root = tree.getroot()
+    subcircuits = root[1]
 
-	devices = defaultdict(set)
-	for sc in subcircuits:
-		for ssc in sc.iter('structure'):
-			name = ssc.attrib['name']
-			if  "MosfetDiodeArray" in name:
-				for device in ssc.iter('device'):
-					devices["MosfetDiode"].add(device.attrib['name'].replace("/", ""))
+    devices = defaultdict(set)
+    for structure in root.findall(".//structure"):
+        name = structure.attrib["name"]
+        if name.startswith("MosfetDiodeArray"):
+            for device in structure.iter("device"):
+                devices["MosfetDiode"].add(device.attrib["name"].replace("/", ""))
 
-	tree = ET.parse(glob.glob(os.path.join(netlist_dir, "partitioning_result.xml"))[0])
-	root = tree.getroot()
-	subcircuits = root[1]
-	for cap in subcircuits.iter('capacitance'):
-		# print (cap.attrib['type'].replace("/", ""))
-		for device in cap.iter('device'):
-			devices[cap.attrib['type'] + "_cap"].add(device.attrib['name'].replace("/", ""))
-	return [{'sub_circuit_name': k, 'transistor_names': list(v)} for k,v in devices.items()]
+    tree = ET.parse(glob.glob(os.path.join(netlist_dir, "partitioning_result.xml"))[0])
+    root = tree.getroot()
+    subcircuits = root[1]
+    for cap in subcircuits.iter("capacitance"):
+        # print (cap.attrib['type'].replace("/", ""))
+        for device in cap.iter("device"):
+            devices[cap.attrib["type"] + "_cap"].add(
+                device.attrib["name"].replace("/", "")
+            )
+    return [
+        {"sub_circuit_name": k, "transistor_names": list(v)} for k, v in devices.items()
+    ]
 
 
 def get_hl2_cluster_labels(netlist_dir="data/netlist1/"):
-	tree = ET.parse(glob.glob(os.path.join(netlist_dir, "structure_result.xml"))[0])
-	root = tree.getroot()
-	subcircuits = root[1]
-	return extract_HL2_devices(subcircuits)
+    tree = ET.parse(glob.glob(os.path.join(netlist_dir, "structure_result.xml"))[0])
+    root = tree.getroot()
+    subcircuits = root[1]
+
+    HL2_subcircuits = []
+    for sc in subcircuits:
+        name = rename(sc.attrib["name"])
+        if name == "DiffPair":
+            should_skip = False
+            for pin in sc[0]:
+                if pin.attrib["net"] == "/vref":
+                    should_skip = True
+                    break
+            if should_skip:
+                continue
+
+        device_names = []
+        for device in sc.iter("device"):
+            device_names.append(device.attrib["name"].replace("/", ""))
+
+        if name not in ["cap", "MosfetDiode", "Mosfet"]:
+            if len(device_names) == 1:
+                print("Warning: Only one device in subcircuit:", name)
+                print(sc.attrib["name"])
+            HL2_subcircuits.append(
+                {"sub_circuit_name": name, "transistor_names": device_names}
+            )
+    return HL2_subcircuits
+
+
+def get_hl3_cluster_labels(netlist_dir="data/asi-fuboco-test"):
+    tree = ET.parse(glob.glob(os.path.join(netlist_dir, "partitioning_result.xml"))[0])
+    root = tree.getroot()
+    partitions = root[1]
+    name_mappings = {
+        "firstStage": "firstStage",
+        "primarySecondStage": "secondStage",
+        "secondarySecondStage": "secondStage",
+        "thirdStage": "secondStage",
+        "fourthStage": "thirdStage",
+    }
+    hl3_clusters = defaultdict(set)
+    for par in partitions:
+        name = par.tag
+        print(par.tag)
+        if par.tag == "gmParts":
+            for gm in par.iter("gmPart"):
+                # print(gm.attrib["type"])
+                partition_type = name_mappings.get(gm.attrib["type"], gm.attrib["type"])
+                print(partition_type)
+
+                for device in gm.iter("device"):
+                    hl3_clusters[partition_type].add(
+                        device.attrib["name"].replace("/", "")
+                    )
+        if par.tag == "loadParts":
+            for loadpart in par.iter("loadPart"):
+                partition_type = "loadPart"
+                for device in loadpart.iter("device"):
+                    hl3_clusters[partition_type].add(
+                        device.attrib["name"].replace("/", "")
+                    )
+
+        if par.tag == "biasParts":
+            for biaspart in par.iter("biasPart"):
+                partition_type = "biasPart"
+                for device in biaspart.iter("device"):
+                    hl3_clusters[partition_type].add(
+                        device.attrib["name"].replace("/", "")
+                    )
+
+        if par.tag == "commonModeSignalDetectorParts":
+            for cmfb in par.iter("commonModeSignalDetectorPart"):
+                partition_type = "commonModeSignalDetectorPart"
+                for device in cmfb.iter("device"):
+                    hl3_clusters[partition_type].add(
+                        device.attrib["name"].replace("/", "")
+                    )
+
+        if par.tag == "positiveFeedbackParts":
+            for fb in par.iter("positiveFeedbackPart"):
+                partition_type = "positiveFeedbackPart"
+                for device in fb.iter("device"):
+                    hl3_clusters[partition_type].add(
+                        device.attrib["name"].replace("/", "")
+                    )
+
+    # hl3_clusters = {k: list(v) for k, v in hl3_clusters.items()}
+    hl3_clusters = [
+        {"sub_circuit_name": k, "transistor_names": list(v)}
+        for k, v in hl3_clusters.items()
+    ]
+    # print(hl3_clusters)
+    return hl3_clusters
+
 
 if __name__ == "__main__":
-	print(get_hl2_cluster_labels(netlist_dir="data/benchmark-asi-100/small/2/"))
+    print(get_hl2_cluster_labels(netlist_dir="data/asi-fuboco-test/small/2/"))
+    print(get_hl3_cluster_labels(netlist_dir="data/asi-fuboco-test/small/1/"))
--- src/netlist.py
+++ src/netlist.py
@@ -1,9 +1,15 @@
 from calc1 import merge_cm_transistor_cluster
-from src.extract_circuit_info import get_hl2_cluster_labels, get_hl1_cluster_labels
+from src.extract_circuit_info import (
+    get_hl1_cluster_labels,
+    get_hl2_cluster_labels,
+    get_hl3_cluster_labels,
+)
 from mask_net import get_masked_netlist
 
-class SPICENetlist():
-    def __init__ (self, netlist_path):
+
+class SPICENetlist:
+    def __init__(self, netlist_path):
         self.netlist = get_masked_netlist(netlist_path, use_meaninful_token=True)
         self.hl1_gt = get_hl1_cluster_labels(netlist_path)
         self.hl2_gt = merge_cm_transistor_cluster(get_hl2_cluster_labels(netlist_path))
+        self.hl3_gt = get_hl3_cluster_labels(netlist_path)
