786fc5cf23efea6a1ad75e9d8f582469b5328d7b
Phuoc Pham <phuocphn@gmail.com>
2025-04-29 10:16:55 +0200

    :zap: update available subcircuit names

---
+++ examples/get_demonstration_examples.py
@@ -0,0 +1,181 @@
+import glob
+import os
+import xml.etree.ElementTree as ET
+
+
+def check_add(list_subcircuits, counter):
+    """Check if any of the subcircuits in could be used as demonstration circuits."""
+    should_add = False
+    for sc in list_subcircuits:
+        if sc in counter and counter[sc] == 0:
+            counter[sc] = 1
+            should_add = True
+    return should_add
+
+
+def get_demonstration_examples(samples):
+    all_examples = []
+    available_subcircuit_names = list(
+        {
+            "MosfetCascodedDifferentialPair",
+            "MosfetImprovedWilsonCurrentMirror",
+            "MosfetSimpleCurrentMirror",
+            "CapacitorArray",
+            "MosfetDiodeArray",
+            "MosfetCascodeAnalogInverterNmosDiodeTransistor",
+            "MosfetCascodeCurrentMirror",
+            "MosfetFoldedCascodeDifferentialPair",
+            "MosfetAnalogInverter",
+            "MosfetDifferentialPair",
+            "MosfetCascodeAnalogInverterNmosCurrentMirrorLoad",
+            "MosfetFourTransistorCurrentMirror",
+            "MosfetCascodeNMOSAnalogInverterOneDiodeTransistor",
+            "MosfetWilsonCurrentMirror",
+            "MosfetCascodedAnalogInverter",
+            "MosfetNormalArray",
+            "MosfetCascodedPMOSAnalogInverter",
+            "MosfetCascodePMOSAnalogInverterOneDiodeTransistor",
+            "MosfetCascodeAnalogInverterPmosDiodeTransistor",
+            "MosfetCascodedNMOSAnalogInverter",
+            "MosfetWideSwingCascodeCurrentMirror",
+            "MosfetNmosDiodeAnalogInverter",
+            "MosfetPmosDiodeAnalogInverter",
+        }
+    )
+    counter = {k: 0 for k in available_subcircuit_names}
+
+    for dir in ["small", "medium"]:
+        for i in samples:  # range(1, 101):
+            netlist_dir = f"data/asi-fuboco-train/{dir}/{i}/"
+            tree = ET.parse(
+                glob.glob(os.path.join(netlist_dir, "structure_result.xml"))[0]
+            )
+            root = tree.getroot()
+            subcircuits = root[1]
+
+            all_subcircuits = []
+            for sc in subcircuits:
+                circuit_name = sc.attrib["name"]
+                circuit_name = circuit_name[: circuit_name.find("[")]
+                all_subcircuits.append(circuit_name)
+
+            if check_add(all_subcircuits, counter):
+                all_examples.append(netlist_dir)
+
+    return all_examples
+
+
+def get_demonstration_examples_v2(samples):
+    all_examples = []
+    available_subcircuit_names = list(
+        {
+            "MosfetCascodedDifferentialPair",
+            "MosfetImprovedWilsonCurrentMirror",
+            "MosfetSimpleCurrentMirror",
+            "CapacitorArray",
+            "MosfetDiodeArray",
+            "MosfetCascodeAnalogInverterNmosDiodeTransistor",
+            "MosfetCascodeCurrentMirror",
+            "MosfetFoldedCascodeDifferentialPair",
+            "MosfetAnalogInverter",
+            "MosfetDifferentialPair",
+            "MosfetCascodeAnalogInverterNmosCurrentMirrorLoad",
+            "MosfetFourTransistorCurrentMirror",
+            "MosfetCascodeNMOSAnalogInverterOneDiodeTransistor",
+            "MosfetWilsonCurrentMirror",
+            "MosfetCascodedAnalogInverter",
+            "MosfetNormalArray",
+            "MosfetCascodedPMOSAnalogInverter",
+            "MosfetCascodePMOSAnalogInverterOneDiodeTransistor",
+            "MosfetCascodeAnalogInverterPmosDiodeTransistor",
+            "MosfetCascodedNMOSAnalogInverter",
+            "MosfetWideSwingCascodeCurrentMirror",
+            "MosfetNmosDiodeAnalogInverter",
+            "MosfetPmosDiodeAnalogInverter",
+        }
+    )
+    counter = {k: 0 for k in available_subcircuit_names}
+
+    for index, i in enumerate(samples):  # range(1, 101):
+        if index < 3:
+            dir = "small"
+        else:
+            dir = "medium"
+        netlist_dir = f"data/asi-fuboco-train/{dir}/{i}/"
+        tree = ET.parse(glob.glob(os.path.join(netlist_dir, "structure_result.xml"))[0])
+        root = tree.getroot()
+        subcircuits = root[1]
+
+        all_subcircuits = []
+        for sc in subcircuits:
+            circuit_name = sc.attrib["name"]
+            circuit_name = circuit_name[: circuit_name.find("[")]
+            all_subcircuits.append(circuit_name)
+
+        if check_add(all_subcircuits, counter):
+            all_examples.append(netlist_dir)
+
+    return all_examples
+
+
+def check_cover(examples):
+    available_subcircuit_names = list(
+        {
+            "MosfetCascodedDifferentialPair",
+            "MosfetImprovedWilsonCurrentMirror",
+            "MosfetSimpleCurrentMirror",
+            "CapacitorArray",
+            "MosfetDiodeArray",
+            "MosfetCascodeAnalogInverterNmosDiodeTransistor",
+            "MosfetCascodeCurrentMirror",
+            "MosfetFoldedCascodeDifferentialPair",
+            "MosfetAnalogInverter",
+            "MosfetDifferentialPair",
+            "MosfetCascodeAnalogInverterNmosCurrentMirrorLoad",
+            "MosfetFourTransistorCurrentMirror",
+            "MosfetCascodeNMOSAnalogInverterOneDiodeTransistor",
+            "MosfetWilsonCurrentMirror",
+            "MosfetCascodedAnalogInverter",
+            "MosfetNormalArray",
+            "MosfetCascodedPMOSAnalogInverter",
+            "MosfetCascodePMOSAnalogInverterOneDiodeTransistor",
+            "MosfetCascodeAnalogInverterPmosDiodeTransistor",
+            "MosfetCascodedNMOSAnalogInverter",
+            "MosfetWideSwingCascodeCurrentMirror",
+            "MosfetNmosDiodeAnalogInverter",
+            "MosfetPmosDiodeAnalogInverter",
+        }
+    )
+    counter = {k: 0 for k in available_subcircuit_names}
+
+    for ex in examples:
+        tree = ET.parse(glob.glob(os.path.join(ex, "structure_result.xml"))[0])
+        root = tree.getroot()
+        subcircuits = root[1]
+
+        all_subcircuits = []
+        for sc in subcircuits:
+            circuit_name = sc.attrib["name"]
+            circuit_name = circuit_name[: circuit_name.find("[")]
+            all_subcircuits.append(circuit_name)
+
+            counter[circuit_name] = 1
+
+    return 0 not in counter.values()
+
+
+if __name__ == "__main__":
+    import random
+
+    for _ in range(20000):
+        # samples = [56, 49, 51]  # random.sample(range(1, 101), 3)
+        samples = random.sample(range(1, 101), 5)
+        demonstration_examples = get_demonstration_examples_v2(samples)
+        cover = check_cover(demonstration_examples)
+
+        if cover:
+            print("found")
+            print(len(demonstration_examples), cover)
+            print(samples)
+            print(demonstration_examples)
+            break
