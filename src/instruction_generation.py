029785225508fb5da73f7bdee4ee823ab2f67f5f
Phuoc Pham <phuocphn@gmail.com>
2025-04-30 13:13:41 +0200

    :art: support instruction generation for all hierarchical levels

--- src/instruction_generation.py
+++ src/instruction_generation.py
@@ -1,42 +1,27 @@
 import glob
 import json
 import os
+import datetime
+import click
 import xml.etree.ElementTree as ET
 from pathlib import Path
+from loguru import logger
 from calc1 import compute_cluster_metrics, average_metrics
 from src.netlist import SPICENetlist
 from prompt_collections.rules import (
     create_gen_rule_prompt,
-    create_update_rule_prompt,
     gen_instruction_prompt,
-    update_instruction_prompt,
+    gen_instruction_hl3_prompt,
     update_instruction_prompt_v2,
     gen_instruction_hl1_prompt,
 )
 
 from prompt_collections.hl2 import (
-    create_prompt_hl2,
-    create_prompt_hl2_multiple_subcircuits_with_rule_provided,
     create_prompt_hl2_multiple_subcircuits_with_rule_provided_v2,
 )
 
-from main import llm_invoke, identify_devices
-from models import (
-    load_ollama,
-    load_openai,
-    load_deepseek,
-)
-
+from models import load_llms
 from utils import ppformat, configure_logging
-
-# -----
-from loguru import logger
-import sys
-import datetime
-
-
-# -----
-from collections import defaultdict
 
 
 def get_demonstration_examples(samples=[56, 49, 51]):
@@ -58,7 +43,9 @@ def get_demonstration_examples(samples=[56, 49, 51]):
                 subcircuit_names.add(subcircuit_name)
             all_examples.append(netlist_dir)
 
-    assert len(subcircuit_names) == 21, f"number of subcircuit: {len(subcircuit_names)}"
+    assert (
+        len(subcircuit_names) == 21 + 2
+    ), f"number of subcircuit: {len(subcircuit_names)}"
     return all_examples
 
 
@@ -129,27 +116,74 @@ def model_call(model, prompt, data: SPICENetlist) -> list[str, str]:
         return None, None
 
 
-if __name__ == "__main__":
-    demonstration_examples = get_demonstration_examples()
+def llm_invoke(model, prompt, data: SPICENetlist) -> list[str, str]:
+    try:
+        # prompt = create_prompt_hl2()
+        logger.info(prompt.invoke(data.netlist).to_string())
+        chain = prompt | model  # | parser
+        output = chain.invoke({"netlist": data.netlist})
+        parsed_data = json.loads(
+            output.content[
+                output.content.find("<json>")
+                + len("<json>") : output.content.find("</json>")
+            ]
+        )
+        assert isinstance(parsed_data, list), "parsed data is not a list: " + str(
+            parsed_data
+        )
+        return output, parsed_data
+    except json.decoder.JSONDecodeError as e:
+        logger.error(f"parsing LLM output failed: " + output.content)
+        return None, None
+
+    except Exception as e:
+        logger.error(f"exception: {e}")
+        return None, None
+
+
+# @click.command()
+# @click.option(
+#     "--model_name",
+#     default="gpt-4.1",
+#     help="the LLM is used for instruction generation stage",
+# )
+# @click.option(
+#     "--subcircuit",
+#     default="HL1",
+#     prompt="which is the target subcircuit ?",
+#     help="which is the target subcircuit ?",
+#     type=click.Choice(
+#         ["HL1", "Current Mirror", "Differential Pair", "Inverter", "HL3"],
+#         case_sensitive=True,
+#     ),
+# )
+def gen_rules(model_name, subcircuit):
+    demonstration_examples = get_demonstration_examples([79, 51, 5])
 
     logger.info(f"len of demonstration examples: {demonstration_examples}")
-    subcircuit = "Current Mirror"
-    subcircuit = "HL1"
     subcircuit_abbrev_map = {
         "Current Mirror": "CM",
         "Differential Pair": "DiffPair",
         "Inverter": "Inverter",
         "HL1": "HL1",
+        "HL3": "HL3",
     }
+    if subcircuit == "HL1":
+        level = 1
+    elif subcircuit == "HL3":
+        level = 3
+    else:
+        level = 2
 
-    model_id = "gpt-4o"
-    model = load_openai(model_id)
-    name = f"{model_id}-{subcircuit_abbrev_map[subcircuit]}-{datetime.datetime.now():%Y-%m-%d_%H:%M:%S}"
+    model = load_llms(model_name)
+    if level == 2:
+        name = f"{model_name}/HL2-{subcircuit_abbrev_map[subcircuit]}"
+    else:
+        name = f"{model_name}/{subcircuit_abbrev_map[subcircuit]}"
 
-    working_dir = f"outputs/rule_accumulator/{name}"
+    working_dir = f"outputs/instruction_generation/{name}"
     Path(working_dir).mkdir(parents=True, exist_ok=True)
     configure_logging(logdir=working_dir)
-    log_level = "DEBUG"
 
     instruction = f"Generate a rule for {subcircuit} subcircuits"
     logger.info(f"Instruction: {instruction}")
@@ -157,20 +191,27 @@ if __name__ == "__main__":
     for index, ex in enumerate(demonstration_examples):
         data = SPICENetlist(ex)
 
-        ground_truth = [
-            sc["transistor_names"]
-            for sc in data.hl2_gt
-            if sc["sub_circuit_name"] == subcircuit_abbrev_map[subcircuit]
-        ]
-        prompt_fn = gen_instruction_hl1_prompt  # or gen_instruction_prompt
-
-        prompt = gen_instruction_prompt(
-            subcircuit_name=subcircuit,
-            netlist=data.netlist,
-            ground_truth=ground_truth,
-        )
-        # or
-        prompt = gen_instruction_hl1_prompt(netlist=data.netlist, hl1_gt=data.hl1_gt)
+        if level == 1:
+            prompt = gen_instruction_hl1_prompt(
+                netlist=data.netlist, hl1_gt=data.hl1_gt
+            )
+            # prompt_fn = gen_instruction_hl1_prompt  # or gen_instruction_prompt
+        elif level == 2:
+            ground_truth = [
+                sc["transistor_names"]
+                for sc in data.hl2_gt
+                if sc["sub_circuit_name"] == subcircuit_abbrev_map[subcircuit]
+            ]
+            prompt = gen_instruction_prompt(
+                subcircuit_name=subcircuit,
+                netlist=data.netlist,
+                ground_truth=ground_truth,
+            )
+        elif level == 3:
+            prompt = gen_instruction_hl3_prompt(
+                netlist=data.netlist,
+                hl3_gt=data.hl3_gt,
+            )
 
         logger.info(prompt.invoke({}).to_string())
 
@@ -191,12 +232,14 @@ if __name__ == "__main__":
             with open(os.path.join(working_dir, fn), "r") as f:
                 prev_instruction = f.read()
 
-            if subcircuit == "HL1":
+            if level == 1:
                 subcircuit_name = (
                     "diode-connected transistors and load/compensation capacitors"
                 )
-            else:
+            elif level == 2:
                 subcircuit_name = subcircuit
+            elif level == 3:
+                subcircuit_name = "amplification stages (first, second, third stage), feedback stage, load and bias parts"
 
             prompt = update_instruction_prompt_v2(
                 subcircuit_name=subcircuit_name,
@@ -212,5 +255,20 @@ if __name__ == "__main__":
             ) as f:
                 f.write(parsed_data)
 
-    with open(os.path.join(working_dir, f"instruction-{index}-revised.md"), "r") as f:
-        final_generated_instruction = f.read()
+    # with open(os.path.join(working_dir, f"instruction-{index}-revised.md"), "r") as f:
+    #     final_generated_instruction = f.read()
+
+
+@click.command()
+@click.option(
+    "--model_name",
+    default="gpt-4.1",
+    help="the LLM is used for instruction generation stage",
+)
+def gen_all_rules(model_name):
+    for subcircuit in ["HL1", "Differential Pair", "Current Mirror", "Inverter", "HL3"]:
+        gen_rules(model_name, subcircuit)
+
+
+if __name__ == "__main__":
+    gen_all_rules()
