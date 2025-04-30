cecf907e94bed6f74f269555a0ebf5e468820d6c
Phuoc Pham <phuocphn@gmail.com>
2025-04-30 13:16:47 +0200

    🎨 feat: add support for code generation of different subcircuits

--- src/code_generation.py
+++ src/code_generation.py
@@ -2,6 +2,7 @@ from prompt_collections.rules import (
     gen_python_script,
     gen_python_script_v2,
     gen_python_script_v3,
+    gen_python_script_v4,
 )
 from src.netlist import SPICENetlist
 import json
@@ -10,33 +11,30 @@ from models import load_openai
 import click
 
 
-@click.command()
-@click.option(
-    "--instruction_path",
-    default="/tmp/abc.txt",
-    help="path to the markdown instruction file.",
-)
-@click.option(
-    "--subcircuit_name",
-    default="Current Mirror",
-    prompt="subcircuit name",
-    help="subcircuit name.",
-)
-@click.option(
-    "--testcase_netlist",
-    default="data/asi-fuboco-train/medium/56",
-    prompt="Test case netlist",
-    help="subcircuit name.",
-)
-@click.option(
-    "--call_model",
-    default=False,
-    prompt="should call model and get the output?",
-    help="enable invoking model (gpt-4o).",
-)
-def gen_prompt(instruction_path, subcircuit_name, testcase_netlist, call_model):
-    # instruction_path = "outputs/rule_accumulator/llama3.3:70b-2025-04-19_14:54:24/instruction-12-revised.md"
-    # subcircuit_name = "Current Mirror"
+demonstration_netlists = {
+    1: ["data/asi-fuboco-train/medium/79/"],
+    2: [
+        "data/asi-fuboco-train/small/79/",
+        "data/asi-fuboco-train/medium/79/",
+    ],
+    4: [
+        "data/asi-fuboco-train/small/51/",
+        "data/asi-fuboco-train/small/79/",
+        "data/asi-fuboco-train/medium/51/",
+        "data/asi-fuboco-train/medium/79/",
+    ],
+    6: [
+        "data/asi-fuboco-train/small/5/",
+        "data/asi-fuboco-train/small/51/",
+        "data/asi-fuboco-train/small/79/",
+        "data/asi-fuboco-train/medium/5/",
+        "data/asi-fuboco-train/medium/51/",
+        "data/asi-fuboco-train/medium/79/",
+    ],
+}
+
+
+def hl2_code_generator(instruction_path, subcircuit_name, test_index, call_model):
     print(f"subcircuit name: {subcircuit_name}")
     print(f"instruction path: {instruction_path}")
 
@@ -49,22 +47,13 @@ def gen_prompt(instruction_path, subcircuit_name, testcase_netlist, call_model):
         "Inverter": "Inverter",
     }
 
-    testcases = [
-        # "data/asi-fuboco-train/small/56/",
-        # "data/asi-fuboco-train/small/49/",
-        "data/asi-fuboco-train/small/51/",
-        # "data/asi-fuboco-train/medium/56/",
-        # "data/asi-fuboco-train/medium/49/",
-        # "data/asi-fuboco-train/medium/51/",
-    ]
     testcase_str = ""
-    for i, testcase_netlist in enumerate(testcases):
-        data = SPICENetlist(testcase_netlist)
+    for i, testcase in enumerate(demonstration_netlists[test_index]):
+        data = SPICENetlist(testcase)
 
         ground_truth = str(
             json.dumps(
                 [
-                    # {"sub_circuit_name": "CM", "transistor_names": sc["transistor_names"]}
                     sc
                     for sc in data.hl2_gt
                     if sc["sub_circuit_name"] == subcircuit_abbrev_map[subcircuit_name]
@@ -89,9 +78,9 @@ def gen_prompt(instruction_path, subcircuit_name, testcase_netlist, call_model):
         """
         testcase_str += test
 
-    prompt = gen_python_script_v3(
+    prompt = gen_python_script_v4(
         instruction=generated_instruction,
-        subcircuit_name=subcircuit_name,
+        subcircuit_name=subcircuit_name + "s",
         testcase=testcase_str,
     )
 
@@ -102,34 +91,14 @@ def gen_prompt(instruction_path, subcircuit_name, testcase_netlist, call_model):
         print(output)
 
 
-@click.command()
-@click.option(
-    "--instruction_path",
-    default="/tmp/abc.txt",
-    help="path to the markdown instruction file.",
-)
-@click.option(
-    "--call_model",
-    default=False,
-    prompt="should call model and get the output?",
-    help="enable invoking model (gpt-4o).",
-)
-def gen_hl1_prompt(instruction_path, call_model):
+def gen_hl1_prompt(instruction_path, test_index, call_model):
     print(f"instruction path: {instruction_path}")
 
     with open(instruction_path, "r") as f:
         generated_instruction = f.read()
 
-    testcases = [
-        # "data/asi-fuboco-train/small/56/",
-        # "data/asi-fuboco-train/small/49/",
-        "data/asi-fuboco-train/small/51/",
-        # "data/asi-fuboco-train/medium/56/",
-        # "data/asi-fuboco-train/medium/49/",
-        # "data/asi-fuboco-train/medium/51/",
-    ]
     testcase_str = ""
-    for i, testcase_netlist in enumerate(testcases):
+    for i, testcase_netlist in enumerate(demonstration_netlists[test_index]):
         data = SPICENetlist(testcase_netlist)
 
         ground_truth = str(
@@ -153,9 +122,9 @@ def gen_hl1_prompt(instruction_path, call_model):
         """
         testcase_str += test
 
-    prompt = gen_python_script_v3(
+    prompt = gen_python_script_v4(
         instruction=generated_instruction,
-        subcircuit_name="diode-connected transistor and load/compensation capacitor",  # no "s" here!
+        subcircuit_name="diode-connected transistors and load/compensation capacitors",
         testcase=testcase_str,
     )
 
@@ -166,5 +135,40 @@ def gen_hl1_prompt(instruction_path, call_model):
         print(output)
 
 
+@click.command()
+@click.option(
+    "--level",
+    default=2,
+    prompt="Hierarchical Level: ",
+)
+@click.option(
+    "--instruction_path",
+    default="/tmp/abc.txt",
+    help="path to the markdown instruction file.",
+)
+@click.option(
+    "--subcircuit_name",
+    default="Current Mirror",
+    prompt="subcircuit name",
+    help="subcircuit name.",
+)
+@click.option(
+    "--test_index",
+    default=1,
+    prompt="Test case index: ",
+)
+@click.option(
+    "--call_model",
+    default=False,
+    prompt="should call model and get the output?",
+    help="enable invoking model (gpt-4o).",
+)
+def code_generator(level, instruction_path, subcircuit_name, test_index, call_model):
+    if level == 1:
+        gen_hl1_prompt(instruction_path, test_index, call_model)
+    if level == 2:
+        hl2_code_generator(instruction_path, subcircuit_name, test_index, call_model)
+
+
 if __name__ == "__main__":
-    gen_prompt()
+    code_generator()
